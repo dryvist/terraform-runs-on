ede1d121dd6a374db6109ca467bbd729c2546cc5
JacobPEvans <20714140+JacobPEvans-personal@users.noreply.github.com>
2026-07-13 10:58:01 -0400

    fix(secrets): consume OpenBao credentials natively

--- provider.tf
+++ provider.tf
@@ -1,5 +1,18 @@
-provider "aws" {
+provider "vault" {}
+
+ephemeral "vault_aws_access_credentials" "this" {
+  mount  = "aws"
+  role   = "tofu-runs-on"
+  type   = "sts"
   region = "us-east-2"
+  ttl    = "1h"
+}
+
+provider "aws" {
+  region     = "us-east-2"
+  access_key = ephemeral.vault_aws_access_credentials.this.access_key
+  secret_key = ephemeral.vault_aws_access_credentials.this.secret_key
+  token      = ephemeral.vault_aws_access_credentials.this.security_token
 
   default_tags {
     tags = local.common_tags
